5c275f223d73636bfa3627624534e296093fefdb
Alex Escalante <alexescalante630@gmail.com>
2024-07-06 23:03:32 -0300

    Los ids de las tablas son incrementables

--- backend/models.py
+++ backend/models.py
@@ -5,13 +5,13 @@ db = SQLAlchemy()
 
 class Genre(db.Model):
     __tablename__ = 'genres'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String[50], nullable=False)
     creation_date = db.Column(db.DateTime, default=datetime.datetime.now())
     
 class Film(db.Model):
     __tablename__ = 'films'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String[50], nullable=False)
     description = db.Column(db.String[500], nullable=False)
     genre_id = db.Column(db.Integer, db.ForeignKey('genres.id'))
@@ -22,7 +22,7 @@ class Film(db.Model):
 
 class Interpreter(db.Model):
     __tablename__ = 'interpreters'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String[50], nullable=False)
     nationality = db.Column(db.String[50])
     birthdate = db.Column(db.Date)
@@ -32,7 +32,7 @@ class Interpreter(db.Model):
 
 class Performance(db.Model):
     __tablename__ = 'performances'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     film_id = db.Column(db.Integer, db.ForeignKey('films.id'))
     interpreter_id = db.Column(db.Integer, db.ForeignKey('interpreters.id'))
     creation_date = db.Column(db.DateTime, default=datetime.datetime.now())
